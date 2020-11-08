b706250af7a843f4c63da438e0cf84edb083b996
Emanuel <emanuel@durmaz.rocks>
2020-11-08 02:44:40 +0100

    rebuilding site So 8. Nov 02:44:40 CET 2020

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ about = document.getElementsByClassName("about")[0]
 ul = about.getElementsByTagName("ul")[0]
 
 h = document.createElement('h2');
-h.textContent = 'M. Sc. in IT-Security';
+h.textContent = 'sometext';
 
 about.insertBefore(h, ul);
 
@@ -21,6 +21,7 @@ ul = about.getElementsByTagName("ul")[0]
 ul.insertAdjacentHTML('afterend', '<p>'+text+'</p>');
 
 
+//Clicky
 var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101286145);
 
 var script = document.createElement('script');
@@ -31,10 +32,8 @@ script.src = "//static.getclicky.com/js";
 
 document.head.appendChild(script);
 
-//<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101286145);</script>
-//<script async src="//static.getclicky.com/js"></script>
 
-
+//Woopra
 (function(){
     var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
 })("woopra");
